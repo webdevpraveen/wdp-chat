bafba1d3a514444433f7c297abf0dd2a44956465
Praveen Singh <pr4veensingh@proton.me>
2025-11-27 01:14:59 +0530

    added backend render url to frontend

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,41 @@ const io = require("socket.io")(http, {
   },
 });
 
+// MEMORY STORE (Temporary)
+let mutedUsers = new Set();
+
 io.on("connection", (socket) => {
   console.log("User connected");
 
+  socket.on("typing", (userId) => {
+    socket.broadcast.emit("userTyping", userId);
+});
+
+socket.on("stopTyping", (userId) => {
+    socket.broadcast.emit("userStopTyping", userId);
+});
+
+  // Normal message
   socket.on("chatMessage", (msg) => {
+    if (mutedUsers.has(msg.userId)) return; 
     io.emit("chatMessage", msg);
+  });
+
+  // ADMIN: Clear chat
+  socket.on("adminClearChat", () => {
+    io.emit("clearChatNow");
+  });
+
+  // ADMIN: Mute all
+  socket.on("adminMuteAll", () => {
+    mutedUsers = new Set(["all"]);
+    io.emit("muteAllNow");
+  });
+
+  // ADMIN: Unmute all
+  socket.on("adminUnmuteAll", () => {
+    mutedUsers = new Set();
+    io.emit("unmuteAllNow");
   });
 
   socket.on("disconnect", () => {
